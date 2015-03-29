11b04123845b386314366d62d32445f587b3e61d
Greg Trowbridge <gjtrowbridge@gmail.com>
2015-03-29 15:38:46 -0700

    Add port variable with correct case (hopefully)

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var server = express();
 server.use('/api', api(express));
 server.use(express.static(__dirname + '/public'));
 
-var port = process.env.port || 8080;
+var port = process.env.PORT || 8080;
 server.listen(port, function() {
   console.log('Server listening on port ' + port);
 });
